48b7bc64aaf20af284007866dff699d3b83a97f6
Tony Gravagno <499473+TonyGravagno@users.noreply.github.com>
2025-09-11 14:32:02 -0700

    Migrate dsconfig.json to .droidscript

--- src/CONSTANTS.js
+++ src/CONSTANTS.js
@@ -1,21 +1,20 @@
-
 const conf = require("../package.json");
 const curVer = Number(conf.version.replace(/^(\d+)\.(\d+)\.(\d+)$/, "$1.$2$3"));
 
 // path to local files and folders
 module.exports = {
-    VERSION: curVer,
-    DEBUG: false,
-    LOCALFOLDER: ".droidscript",
-    DSCONFIG: "dsconfig.json",
-    SAMPLES: ".droidscript/samples",
-    DEFINITIONS: ".droidscript/definitions",
-    PORT: "8088",
-    SAMPLE_PORT: "8018",
-    defPaths: [
-        ".edit/docs/definitions/ts/",
-        ".edit/docs/definitions/",
-        ".edit/definitions/ts/",
-        ".edit/definitions/"
-    ]
+  VERSION: curVer,
+  DEBUG: false,
+  LOCALFOLDER: ".droidscript",
+  DSCONFIG_FILE: ".droidscript/dsconfig.json",
+  SAMPLES: ".droidscript/samples",
+  DEFINITIONS: ".droidscript/definitions",
+  PORT: "8088",
+  SAMPLE_PORT: "8018",
+  defPaths: [
+    ".edit/docs/definitions/ts/",
+    ".edit/docs/definitions/",
+    ".edit/definitions/ts/",
+    ".edit/definitions/",
+  ],
 };
--- src/local-data.js
+++ src/local-data.js
@@ -7,85 +7,105 @@ const CONSTANTS = require("./CONSTANTS");
 
 /** @type {DSCONFIG_T} */
 const data = {
-    VERSION: CONSTANTS.VERSION,
-    serverIP: '',
-    serverIPs: [],
-    reload: '',
-    PORT: CONSTANTS.PORT,
-    localProjects: [],
-    info: {},
+  VERSION: CONSTANTS.VERSION,
+  serverIP: "",
+  serverIPs: [],
+  reload: "",
+  PORT: CONSTANTS.PORT,
+  localProjects: [],
+  info: {},
 };
 
 function load() {
-    if (global._dsconf_data) return global._dsconf_data;
-    const filePath = path.join(os.homedir(), CONSTANTS.DSCONFIG);
+  if (global._dsconf_data) return global._dsconf_data;
+  migrateConfigFile();
+  const filePath = path.join(os.homedir(), CONSTANTS.DSCONFIG_FILE);
+  if (fs.existsSync(filePath)) {
+    const fileData = fs.readFileSync(filePath, "utf8");
+    Object.assign(data, JSON.parse(fileData));
 
-    if (fs.existsSync(filePath)) {
-        const fileData = fs.readFileSync(filePath, 'utf8');
-        Object.assign(data, JSON.parse(fileData));
+    if (data.VERSION != CONSTANTS.VERSION)
+      console.log("version change", data.VERSION, "->", CONSTANTS.VERSION);
+    adjust(data);
+  }
 
-        if (data.VERSION != CONSTANTS.VERSION) console.log("version change", data.VERSION, '->', CONSTANTS.VERSION);
-        adjust(data);
-    }
+  return (global._dsconf_data = data);
+}
 
-    return global._dsconf_data = data;
+/**
+ * v0.3.6+, ~/dsconfig.json migrates to ~/.droidscript/dsconfig.json
+ */
+function migrateConfigFile() {
+  const oldFilePath = path.join(os.homedir(), "dsconfig.json");
+  if (!fs.existsSync(oldFilePath)) return;
+  const newFilePath = path.join(os.homedir(), CONSTANTS.DSCONFIG_FILE);
+  fs.moveSync(oldFilePath, newFilePath);
 }
 
 /** @param {DSCONFIG_T} CONFIG */
 function save(CONFIG = data) {
-    if (CONFIG != data) console.error("invalid data");
-    const strdata = JSON.stringify(adjust(CONFIG), null, 2);
-    const filePath = path.join(os.homedir(), CONSTANTS.DSCONFIG);
-    fs.writeFileSync(filePath, strdata);
+  if (CONFIG != data) console.error("invalid data");
+  const strdata = JSON.stringify(adjust(CONFIG), null, 2);
+  const filePath = path.join(os.homedir(), CONSTANTS.DSCONFIG_FILE);
+  fs.writeFileSync(filePath, strdata);
 }
 
 /** @param {DSCONFIG_T} config */
 function adjust(config) {
-    if (!config.localProjects) config.localProjects = [];
-    if (!config.info) config.info = {};
-    if (!config.serverIPs) config.serverIPs = [];
+  if (!config.localProjects) config.localProjects = [];
+  if (!config.info) config.info = {};
+  if (!config.serverIPs) config.serverIPs = [];
 
-    config.serverIPs = config.serverIPs
-        .map(h => {
-            const [host, port] = h.replace(/^https?:\/\//, '').split(':');
-            return `${host}:${port || config.PORT || CONSTANTS.PORT}`;
-        })
-        .filter((h, i, a) => h && a.indexOf(h) === i);
+  config.serverIPs = config.serverIPs
+    .map((h) => {
+      const [host, port] = h.replace(/^https?:\/\//, "").split(":");
+      return `${host}:${port || config.PORT || CONSTANTS.PORT}`;
+    })
+    .filter((h, i, a) => h && a.indexOf(h) === i);
 
-    // normalize serverIP and seed recent list
-    if (config.serverIP) {
-        config.serverIP = config.serverIP.replace(/(https?:\/\/)?([^:]+)(:(\d+))?/,
-            (_, r = "http://", host = "", _p, p = config.PORT) => {
-                config.PORT = p;
-                const endpoint = `${host}:${p}`;
-                config.serverIPs = [endpoint, ...config.serverIPs.filter(h => h !== endpoint)];
-                return `${r}${endpoint}`;
-            });
-    }
+  // normalize serverIP and seed recent list
+  if (config.serverIP) {
+    config.serverIP = config.serverIP.replace(
+      /(https?:\/\/)?([^:]+)(:(\d+))?/,
+      (_, r = "http://", host = "", _p, p = config.PORT) => {
+        config.PORT = p;
+        const endpoint = `${host}:${p}`;
+        config.serverIPs = [
+          endpoint,
+          ...config.serverIPs.filter((h) => h !== endpoint),
+        ];
+        return `${r}${endpoint}`;
+      }
+    );
+  }
 
-    config.serverIPs = config.serverIPs.slice(0, 10);
+  config.serverIPs = config.serverIPs.slice(0, 10);
 
-    config.localProjects = config.localProjects.filter(m => m?.path && fs.existsSync(m.path));
-    for (const p of config.localProjects) p.path = path.resolve(p.path);
-    return config;
+  config.localProjects = config.localProjects.filter(
+    (m) => m?.path && fs.existsSync(m.path)
+  );
+  for (const p of config.localProjects) p.path = path.resolve(p.path);
+  return config;
 }
 
 /**
  * @param {Parameters<LocalProject[]['find']>[0]} filter
  * @param {DSCONFIG_T} CONFIG
  */
-const getProject = (filter, CONFIG = data) => CONFIG.localProjects.find(filter)
+const getProject = (filter, CONFIG = data) => CONFIG.localProjects.find(filter);
 
 /**
  * @param {string} name
  * @param {DSCONFIG_T} CONFIG
  */
-const getProjectByName = (name, CONFIG = data) => CONFIG.localProjects.find(p => p.PROJECT == name)
+const getProjectByName = (name, CONFIG = data) =>
+  CONFIG.localProjects.find((p) => p.PROJECT == name);
 
 /**
  * @param {string} file
  * @param {DSCONFIG_T} CONFIG
  */
-const getProjectByFile = (file, CONFIG = data) => CONFIG.localProjects.find(p => file.startsWith(p.path + path.sep))
+const getProjectByFile = (file, CONFIG = data) =>
+  CONFIG.localProjects.find((p) => file.startsWith(p.path + path.sep));
 
 module.exports = { load, save, getProject, getProjectByName, getProjectByFile };
